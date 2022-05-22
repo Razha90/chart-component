f114684006f9bfe057d40fc7b7a683b7e06f61b4
razha123 <razha9046123@gmail.com>
2022-05-22 08:02:10 -0700

    Upload File

---
+++ script.js
@@ -0,0 +1,50 @@
+var varia = document.getElementsByClassName('value');
+
+function clWed() {
+    varia[2].style.backgroundColor = 'hsl(25, 47%, 15%)';
+}
+function onWed() {
+    varia[2].style.backgroundColor = 'hsl(33, 100%, 98%)';
+}
+
+function onMon() {
+    varia[0].style.backgroundColor = 'hsl(25, 47%, 15%)';
+}
+function clMon() {
+    varia[0].style.backgroundColor = 'hsl(33, 100%, 98%)';
+}
+
+function onTue() {
+    varia[1].style.backgroundColor = 'hsl(25, 47%, 15%)';
+}
+function clTue() {
+    varia[1].style.backgroundColor = 'hsl(33, 100%, 98%)';
+}
+
+function onThu() {
+    varia[3].style.backgroundColor = 'hsl(25, 47%, 15%)';
+}
+function clThu() {
+    varia[3].style.backgroundColor = 'hsl(33, 100%, 98%)';
+}
+
+function onFri() {
+    varia[4].style.backgroundColor = 'hsl(25, 47%, 15%)';
+}
+function clFri() {
+    varia[4].style.backgroundColor = 'hsl(33, 100%, 98%)';
+}
+
+function onSat() {
+    varia[5].style.backgroundColor = 'hsl(25, 47%, 15%)';
+}
+function clSat() {
+    varia[5].style.backgroundColor = 'hsl(33, 100%, 98%)';
+}
+
+function onSun() {
+    varia[6].style.backgroundColor = 'hsl(25, 47%, 15%)';
+}
+function clSun() {
+    varia[6].style.backgroundColor = 'hsl(33, 100%, 98%)';
+}
